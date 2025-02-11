f820fbfb06e3e4e7ef61862c66a70e890b20ba96
AIMTEC\jurv <vlastimil.juracka@aimtec.cz>
2025-02-11 15:40:28 +0100

    Change

--- AspireApp1.ServiceDefaults/obj/Debug/net8.0/AspireApp1.ServiceDefaults.AssemblyInfo.cs
+++ AspireApp1.ServiceDefaults/obj/Debug/net8.0/AspireApp1.ServiceDefaults.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b407985edaf824b0b66a45899c30149a7af663d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
